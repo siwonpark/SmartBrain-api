d98700f60a20f8e0de12a392b31715ca969a73bf
siwonpark <siwonsiwon80@hotmail.com>
2019-08-31 01:09:44 -0400

    finished all endpoints

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/', (req, res) => {
 app.post('/signin', (req, res) => {
     if (req.body.email === database.users[0].email && 
         req.body.password === database.users[0].password) {
-        res.json('success');
+        res.json(database.users[0]);
     } else {
         res.status(400).json('error logging in');
     }
@@ -50,7 +50,6 @@ app.post('/register', (req, res) => {
         id: '125',
         name: name, 
         email: email, 
-        password: password, 
         entries: 0, 
         joined: new Date()
     })
@@ -71,7 +70,7 @@ app.get('/profile/:id', (req, res) => {
     }
 })
 
-app.post('/image', (req, res) => {
+app.put('/image', (req, res) => {
     const { id } = req.body;
     let found = false;
     database.users.forEach(user => {
