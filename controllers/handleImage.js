95855c61243f5d36f3b219812a316c6f2c446b0c
siwonpark <siwonsiwon80@hotmail.com>
2019-09-26 14:47:59 -0400

    added controllers folder

---
+++ controllers/handleImage.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '52c8326cd26646ec8acd2fbba807df23'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json("unable to use API"))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
